06e62a271c43144db712963fd1ef9b43c1f396df
mdashikulislam <ashik.nwu@gmail.com>
2021-06-12 03:43:49 +0600

    create design

--- resources/views/backend/property/create.blade.php
+++ resources/views/backend/property/create.blade.php
@@ -1,4 +1,128 @@
 @extends('backend.layouts.master')
 @section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Add New Property</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="inputName">Property Title</label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="summernote">Property Description</label>
+                        <textarea id="summernote" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputName">Price</label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>City</label>
+                        <select class="form-control select2" style="width: 100%;">
+                            <option selected="selected">Alabama</option>
+                            <option>Alaska</option>
+                            <option>California</option>
+                            <option>Delaware</option>
+                            <option>Tennessee</option>
+                            <option>Texas</option>
+                            <option>Washington</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputName">Region</label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputName">Rooms</label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputName">Bathrooms</label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputName">Land Area</label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputName">Land Area</label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputName">Living Space</label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputName">Balcony/Terrace Area
+                        </label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+        <div class="col-md-4">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">General</h3>
 
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="inputName">Project Name</label>
+                        <input type="text" id="inputName" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputDescription">Project Description</label>
+                        <textarea id="inputDescription" class="form-control" rows="4"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputStatus">Status</label>
+                        <select id="inputStatus" class="form-control custom-select">
+                            <option selected disabled>Select one</option>
+                            <option>On Hold</option>
+                            <option>Canceled</option>
+                            <option>Success</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputClientCompany">Client Company</label>
+                        <input type="text" id="inputClientCompany" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputProjectLeader">Project Leader</label>
+                        <input type="text" id="inputProjectLeader" class="form-control">
+                    </div>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+    </div>
 @endsection
+@push('js')
+    <script src="{{asset('backend/plugins/select2/js/select2.full.min.js')}}"></script>
+    <script>
+        $('#summernote').summernote({
+            height:400
+        });
+        $('.select2').select2()
+    </script>
+@endpush
+@push('css')
+    <link rel="stylesheet" href="{{asset('backend/plugins/select2/css/select2.min.css')}}">
+@endpush
