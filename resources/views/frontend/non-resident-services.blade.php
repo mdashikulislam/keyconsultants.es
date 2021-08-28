3aacc0db61ece92dd1191e4d49db7900bbea2c03
mdashikulislam <ashik.nwu@gmail.com>
2021-08-28 13:54:47 +0600

    calculator

--- resources/views/frontend/non-resident-services.blade.php
+++ resources/views/frontend/non-resident-services.blade.php
@@ -3,51 +3,11 @@
     NON-RESIDENT TAXES
 @endsection
 @section('content')
-    <div class="content-area" >
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-10 offset-lg-1 col-12">
-                    <div class="property-content">
-                        <p>
-                            At&nbsp;<strong>Key Consultants Mallorca</strong>&nbsp; we are pleased to offer you our
-                            outstanding Administración de Fincas – a complete professional service for your
-                            community with a personal touch.
-                        </p>
-                        <p>
-                            We are not just administrators of your community, we want you to think that we are part
-                            of it.&nbsp; We will assist the president and works committee members in every way we
-                            can to ensure the correct running of your community. We have invested in the latest IT
-                            equipment and providing an online portal that can be accessed 24/7 by every property
-                            owner within your &nbsp;community. In the portal you can see your co-efficient, access
-                            your accounts, change your bank details, report a complaint and much much more.
-                        </p>
-                        <div class="text-center mt-50">
-                            <a href="{{route('contact')}}" class="contact-btn">Get Quote</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="callto-action-area" style="background: url('{{asset('frontend/assets/images/bg/cta.webp')}}') no-repeat center center fixed">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-7">
-                    <h2>WORKING TOGETHER TO ENSURE YOUR YOU GET THE BEST ADVICE</h2>
-                    <p>
-                        As apart of our service, we will always ensure that we provide you with transparency of
-                        service, ensuring that you receive the best, guaranteed cost effective and quality assured
-                        service possible.
-                    </p>
-                    <div class="devaidar"></div>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <div class="content-area" style="padding-bottom: 80px">
         <div class="container">
             <div class="row">
-                <div class="col-lg-9">
+                <div class="col-lg-8">
                     <div class="module">
                        <div class="txt">
                            <h1 class="text-center mb-4">Non-Resident Taxes  -  (Modelo 210)</h1>
@@ -119,7 +79,47 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3"></div>
+                <div class="col-lg-4">
+                    <form action="" id="calculator">
+                        <div class="calculator">
+                        <h4>Calculate your non-resident taxes</h4>
+                        <div class="models">
+                            <p class="cal-text">NON RESIDENT TAX CALCULATOR</p>
+                            <div class="form-group group-sec">
+                                <label for="">Please enter the catastral Value if your Property</label>
+                                <input id="amount" name="amount" type="number" class="form-control">
+                            </div>
+                            <div class="form-group radio-b text-center">
+                                <p>Has the catastral Value been revised in the last 10 years</p>
+                                <input value="YES" checked  type="radio" id="test3" name="year">
+                                <label for="test3">YES</label>
+                                <input value="NO" type="radio" id="test4" name="year">
+                                <label for="test4">NO</label>
+                            </div>
+                            <div class="form-group radio-b text-center">
+                                <p>Are you a resident in a country within the Europe Union?</p>
+                                <input value="YES" checked type="radio" id="test" name="country">
+                                <label for="test">YES</label>
+                                <input value="NO" type="radio" id="test2" name="country">
+                                <label for="test2">NO</label>
+                            </div>
+                            <div class="text-center">
+                                <p style="font-weight: bold">TOTAL TAX TO PAY</p>
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" style="font-weight:bold;" id="basic-addon1">€</span>
+                                </div>
+                                <input id="total" type="text" disabled class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="form-group text-center">
+                                <button class="btn btn-success">Calculate</button>
+                                <button type="reset" class="btn btn-dark">Reset</button>
+                            </div>
+                        </div>
+                    </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
@@ -130,7 +130,7 @@
                     <div class="content">
                         <div class="faq">
                             <h2>Frequently Asked Questions</h2>
-                            <h3 class="mt-3"><i class="fa fa-hand-o-right fa-fw"></i>How are non-resident taxes calculated?</h3>
+                            <h3 class="mt-3"><i class="fa fa-hand-o-right fa-fw" style="color: #c0b298"></i>How are non-resident taxes calculated?</h3>
                             <ul style="list-style-type:square;margin-left: 15px;" class="mt-3">
                                 <li>This depends on whether you are tax resident in another EU Country and whether the Valor Cadastral has been reviewed in the last 10 years.</li>
                                 <li>If your valor cadastral HAS been reviewed in the last 10 years, your tax will be calculated on the following basis:-</li>
@@ -139,39 +139,39 @@
                             </ul>
                         </div>
                         <div class="tax">
-                            <h3 class="mt-3"><i class="fa fa-hand-o-right fa-fw"></i>Where a revision has taken place in the last 10 years </h3>
+                            <h3 class="mt-3"><i class="fa fa-hand-o-right fa-fw" style="color: #c0b298"></i>Where a revision has taken place in the last 10 years </h3>
                             <ul style="list-style-type: disc;margin-left: 15px;" >
                                 <li>100.000€ x 1.1% x 19% = 209€ (for EU Tax Residents) </li>
                                 <li>100.000€ x 1.1% x 24% = 264€ (for NON-EU Tax residents)</li>
                             </ul>
                         </div>
                         <div class="tax">
-                            <h3 class="mt-3"><i class="fa fa-hand-o-right fa-fw"></i>Where NO revision has taken place within 10 years </h3>
+                            <h3 class="mt-3"><i class="fa fa-hand-o-right fa-fw" style="color: #c0b298"></i>Where NO revision has taken place within 10 years </h3>
                             <ul style="list-style-type: disc;margin-left: 15px;">
                                 <li>100.000€ x 2% x 19%  = 380.00  (for EU Tax Residents) </li>
                                 <li>100.000€ x 2% x 24%  = 480.00  (For NON-EU Tax residents) </li>
                             </ul>
                         </div>
                         <div class="link mt-2">
-                            <p>You can check to see if your valor cadastral has been revised in the last 10 years by clicking on this link here:
-                                <a style="word-break: break-all;display: block" target="_blank" href="https://www.agenciatributaria.es/AEAT.internet/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2020/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2010_2020/7_3_4__Municipios_con_valor_catastral_revisado_2010_2020.html">https://www.agenciatributaria.es/AEAT.internet/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2020/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2010_2020/7_3_4__Municipios_con_valor_catastral_revisado_2010_2020.html</a></p>
+                            <p class="mb-2">You can check to see if your valor cadastral has been revised in the last 10 years by clicking on this link here:
+                            <a style="word-break: break-all;display: block" target="_blank" href="https://www.agenciatributaria.es/AEAT.internet/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2020/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2010_2020/7_3_4__Municipios_con_valor_catastral_revisado_2010_2020.html">https://www.agenciatributaria.es/AEAT.internet/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2020/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2010_2020/7_3_4__Municipios_con_valor_catastral_revisado_2010_2020.html</a></p>
                         </div>
-                        <div class="text">
-                            <h3><i class="fa fa-hand-o-right fa-fw"></i>How do I know if my Valor catasral has been reviewed in the last 10 years?</h3>
+                        <div class="text pb-50">
+                            <h3><i class="fa fa-hand-o-right fa-fw" style="color: #c0b298"></i>How do I know if my Valor catasral has been reviewed in the last 10 years?</h3>
                             <p>List here is of Catastral revisions before 10 years:</p>
                             <a href="https://www.agenciatributaria.es/AEAT.internet/va_es/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2019/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019/7_3_4_7__Baleares/7_3_4_7__Baleares.html" style="word-break: break-all;display: block">https://www.agenciatributaria.es/AEAT.internet/va_es/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2019/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019/7_3_4_7__Baleares/7_3_4_7__Baleares.html</a>
-                            <a style="word-break: break-all;display: block"  href="https://www.agenciatributaria.es/AEAT.internet/va_es/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2019/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019.html">https://www.agenciatributaria.es/AEAT.internet/va_es/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2019/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019.html</a>
-                            <p>note that if your valor catastral has not been revised in the last 10 years, the amount of tax imputation tax will be 2%</p>
-                            <h3><i class="fa fa-hand-o-right fa-fw"></i>I pay through my bank and do not have the Valor Cadastral details. </h3>
+                            <a class="mt-2" style="word-break: break-all;display: block"  href="https://www.agenciatributaria.es/AEAT.internet/va_es/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2019/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019.html">https://www.agenciatributaria.es/AEAT.internet/va_es/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_de_ayuda_a_la_presentacion/Ejercicio_2019/_Ayuda_Modelo_100/7__Cumplimentacion_IRPF__I_/7_3__Rendimientos_derivados_de_inmuebles/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019/7_3_4__Municipios_con_valor_catastral_revisado_2009_2019.html</a>
+                            <p class="mt-2">note that if your valor catastral has not been revised in the last 10 years, the amount of tax imputation tax will be 2%</p>
+                            <h3><i class="fa fa-hand-o-right fa-fw" style="color: #c0b298"></i>I pay through my bank and do not have the Valor Cadastral details. </h3>
                             <p>If you pay your IBI and basura via direct debit and have not received a copy of your bill with the valor catastral,, there are a number of ways that you can obtain this information. </p>
                             <ul>
                                 <li> 1)	You can obtain that information from the bank payment slip from your online banking or by requesting this information directly. This sheet will provide you with a CSV Reference Number. You can obtain the full details by following this link and entering the CSV number on this web page.
-                                    <a href="https://sede.tributostenerife.es/sta/CarpetaPublic/Public?APP_CODE=STA&PAGE_CODE=VALDOCS">https://sede.tributostenerife.es/sta/CarpetaPublic/Public?APP_CODE=STA&PAGE_CODE=VALDOCS</a></li>
+                                    <a class="d-block" href="https://sede.tributostenerife.es/sta/CarpetaPublic/Public?APP_CODE=STA&PAGE_CODE=VALDOCS">https://sede.tributostenerife.es/sta/CarpetaPublic/Public?APP_CODE=STA&PAGE_CODE=VALDOCS</a></li>
                                 <li>2)	Request the IBI Payment Receipts from your local Ayuntamiento directly (most Ayuntamientos now accept email requests).</li>
                                 <li>3)	Or, if you have a digital signature you can register online with your Local Ayuntamiento and download your previous IBI receipts. Here is an example link from Calvia Ayuntamiento
-                                    <a href="https://www.admonline.calvia.com/cct/">https://www.admonline.calvia.com/cct/</a> </li>
+                                    <a class="d-block" href="https://www.admonline.calvia.com/cct/">https://www.admonline.calvia.com/cct/</a> </li>
                             </ul>
-                            <h3><i class="fa fa-hand-o-right fa-fw"></i>What happens if I do not pay my Non-Resident Taxes.</h3>
+                            <h3 class="mt-2"><i class="fa fa-hand-o-right fa-fw" style="color: #c0b298"></i>What happens if I do not pay my Non-Resident Taxes.</h3>
                             <p>If you do not submit a non-resident tax return, you will likely have to pay these fees when you come to sell you property, together with any fines or interest that have accrued during the period. </p>
                         </div>
                     </div>
@@ -179,7 +179,25 @@
             </div>
         </div>
     </div>
+    <div class="callto-action-area" style="background: url('{{asset('frontend/assets/images/bg/cta.webp')}}') no-repeat center center fixed">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <h2>WORKING TOGETHER TO ENSURE YOUR YOU GET THE BEST ADVICE</h2>
+                    <p>
+                        As apart of our service, we will always ensure that we provide you with transparency of
+                        service, ensuring that you receive the best, guaranteed cost effective and quality assured
+                        service possible.
+                    </p>
+                    <div class="devaidar"></div>
+                </div>
+            </div>
+        </div>
+    </div>
     <style>
+        .error-message{
+            color: red;
+        }
         .box{
             margin-top: 30px;
             border: 1px solid #0c84ff;
@@ -203,5 +221,82 @@
             font-size: 28px;
             margin-bottom: 30px;
         }
+        .calculator{
+            border: 1px solid #0c84ff;
+            border-radius: 10px;
+            padding: 20px;
+        }
+        .calculator h4{
+            font-size: 22px;
+            text-align: center;
+        }
+        .calculator .models p.cal-text{
+            display: block;
+            font-size: 12px;
+            margin-top: 18px;
+            text-align: center;
+        }
+        .calculator .group-sec label{
+            font-size: 12px;
+            text-align: center;
+        }
+
+         .radio-b label {
+            display: inline-block;
+            padding: 5px 15px;
+            cursor: pointer;
+             border: 1px solid #ced4da;
+             border-radius: 4px;
+             font-weight: bold;
+        }
+        .radio-b input[type="radio"] {
+            display: none;
+        }
+
+        .radio-b input[type="radio"]:checked + label {
+            border: solid 1px #c0b298;
+            background: #c0b298;
+            color: #fff;
+        }
     </style>
 @endsection
+@push('js')
+    <script src="//cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
+    <script>
+        $('#calculator').validate({
+            errorElement: 'span',
+            errorClass: 'error-message',
+            rules: {
+                amount: {
+                    required: true,
+                },
+            },
+            messages: {
+                amount: 'Amount can not be empty',
+            },
+            submitHandler:function (e){
+                event.preventDefault();
+                var amount = $('#amount');
+                var year = $("input[name='year']:checked");
+                var country = $("input[name='country']:checked");
+                var totalAmount = 0;
+                var countryVal = 1;
+                if (year.val() === 'YES'){
+                    if (country.val() === 'YES'){
+                        totalAmount = parseInt(amount.val()) + (Math.round((parseInt(amount.val()) * 1.1 * 19) / 100));
+                    }else{
+
+                    }
+                }else{
+
+                }
+                $('#total').val(new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(totalAmount))
+            }
+        });
+        function addCommas(x) {
+            var parts = x.toString().split(".");
+            parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            return parts.join(".");
+        }
+    </script>
+@endpush
